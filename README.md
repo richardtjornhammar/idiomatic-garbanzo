3f2b06ece044e2cbe4c33036ad9348f7145b1107
Richard Tjörnhammar <richard.tjornhammar@gmail.com>
2021-07-08 10:52:26 +0200

    fib

--- README.md
+++ README.md
@@ -5,12 +5,17 @@ produces the output
 ```
 The factorial of 5 is:
 120
-55
+"These are some Fibonacci numbers"
+(1,1,2,3,5,8,13,21,34,55,89,144)
+"The following is true: (fibbonacci number i)^2 + (fibbonacci number i+1)^2 = (fibbonacci number 2*i+1)"
+("One such case ",3,"^2 + ",5,"^2 = ",34)
+("This is also true",True)
+("This is also true",True)
 "The first Fermat numbers are"
-(5," and are they prime?",True)
-(17," and are they prime?",True)
-(257," and are they prime?",True)
-(65537," and are they prime?",True)
-(4294967297," and are they prime?",False)
-(18446744073709551617," and are they prime?",False)
+(5," and is it prime?",True)
+(17," and is it prime?",True)
+(257," and is it prime?",True)
+(65537," and is it prime?",True)
+(4294967297," and is it prime?",False)
+(18446744073709551617," and is it prime?",False)
 ```
--- src/guard_factorial.hs
+++ src/guard_factorial.hs
@@ -36,6 +36,15 @@ isItPrime n = do
                       let lM05 = log( fromInteger m ) * 0.5
                       isItPrime_rec (n, m, p, lM05)
 
+--https://gist.github.com/richardtjornhammar/ef1719ab0dc683c69d5a864cb05c5a90
+fibonacciTruth :: Integer -> Bool
+fibonacciTruth i = do
+    let fi_0 = ipow ( fibonacci(i) , 2 ) 
+    let fi_1 = ipow ( fibonacci(i+1) , 2 )
+    if fi_0+fi_1 == fibonacci(2*i+1) then
+        True
+    else do
+        False
 
 fermatnumber :: Integer -> Integer
 fermatnumber i = 1+ipow( 2, ipow (2, i) )
@@ -43,33 +52,36 @@ fermatnumber i = 1+ipow( 2, ipow (2, i) )
 main = do 
    putStrLn "The factorial of 5 is:"  
    print ( factorial 5 )
-   print ( fibonacci (10) )
+   print ( "These are some Fibonacci numbers")
+   print ( fibonacci (1), fibonacci(2), fibonacci(3),fibonacci(4),fibonacci(5),fibonacci(6),fibonacci(7),fibonacci(8),fibonacci(9),fibonacci(10),fibonacci(11),fibonacci(12) )
+   print ( "The following is true: (fibbonacci number i)^2 + (fibbonacci number i+1)^2 = (fibbonacci number 2*i+1)")
+   print ( "One such case ", fibonacci(4),"^2 + ",fibonacci(5),"^2 = ",fibonacci(9) )
+   print ( "This is also true",fibonacciTruth (11) )
+   print ( "This is also true",fibonacciTruth (18) )
+
    print ( "The first Fermat numbers are" )
    let i_  = 1
    let f_i = fermatnumber i_
-   print ( f_i ," and are they prime?", isItPrime(f_i))
+   print ( f_i ," and is it prime?", isItPrime(f_i))
 
    let i_  = 2
    let f_i = fermatnumber i_
-   print ( f_i ," and are they prime?", isItPrime(f_i))
+   print ( f_i ," and is it prime?", isItPrime(f_i))
 
    let i_  = 3
    let f_i = fermatnumber i_
-   print ( f_i ," and are they prime?", isItPrime(f_i))
+   print ( f_i ," and is it prime?", isItPrime(f_i))
 
    let i_  = 4
    let f_i = fermatnumber i_
-   print ( f_i ," and are they prime?", isItPrime(f_i))
+   print ( f_i ," and is it prime?", isItPrime(f_i))
 
    let i_  = 5
    let f_i = fermatnumber i_
-   print ( f_i ," and are they prime?", isItPrime(f_i))
+   print ( f_i ," and is it prime?", isItPrime(f_i))
 
    let i_  = 6
    let f_i = fermatnumber i_
-   print ( f_i ," and are they prime?", isItPrime(f_i))
+   print ( f_i ," and is it prime?", isItPrime(f_i))
 
-   let i_  = 7
-   let f_i = fermatnumber i_
-   print ( f_i ," and are they prime?", isItPrime(f_i))
 
